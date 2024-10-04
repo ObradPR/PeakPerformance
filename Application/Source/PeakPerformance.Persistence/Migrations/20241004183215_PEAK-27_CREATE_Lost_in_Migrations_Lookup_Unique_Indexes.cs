b4996bc5874f6623973a1b05bdeab89999a3cb5c
ObradPR <obradpualic@gmail.com>
2024-10-04 20:37:15 +0200

    PEAK-27 - Code Cleanup 1.0 / 1.1 - Migration Creation of Lookup Lost Unique Indexes

---
+++ Application/Source/PeakPerformance.Persistence/Migrations/20241004183215_PEAK-27_CREATE_Lost_in_Migrations_Lookup_Unique_Indexes.cs
@@ -0,0 +1,54 @@
+﻿#nullable disable
+
+namespace PeakPerformance.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class PEAK27_CREATE_Lost_in_Migrations_Lookup_Unique_Indexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ActionTypes_lu_Name",
+                table: "ActionTypes_lu",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SystemRoles_lu_Name",
+                table: "SystemRoles_lu",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+               name: "IX_InjuryTypes_lu_Name",
+               table: "InjuryTypes_lu",
+               column: "Name",
+               unique: true);
+
+            migrationBuilder.CreateIndex(
+               name: "IX_MeasurementUnits_lu_Name",
+               table: "MeasurementUnits_lu",
+               column: "Name",
+               unique: true);
+
+            migrationBuilder.CreateIndex(
+               name: "IX_SocialMediaPlatforms_lu_Name",
+               table: "SocialMediaPlatforms_lu",
+               column: "Name",
+               unique: true);
+
+            migrationBuilder.CreateIndex(
+               name: "IX_TrainingGoals_lu_Name",
+               table: "TrainingGoals_lu",
+               column: "Name",
+               unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
