53ddd21194f76e488be7011e2530a304948bebf2
ObradPR <obradpualic@gmail.com>
2024-11-24 12:58:40 +0100

    PEAK-64 - HUB Challenges - migration - Audit Triggers + Indexes

---
+++ Application/Source/PeakPerformance.Persistence/Migrations/20241124114830_PEAK-64_ADD_Triggers_Indexes_Challenge_ChallengeParticipant.cs
@@ -0,0 +1,30 @@
+﻿#nullable disable
+
+namespace PeakPerformance.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class PEAK64_ADD_Triggers_Indexes_Challenge_ChallengeParticipant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Audit triggers
+
+            migrationBuilder.CreateAuditTriggersForTable<Challenge_aud, Challenge>();
+            migrationBuilder.CreateAuditTriggersForTable<ChallengeParticipant_aud, ChallengeParticipant>();
+
+            // Indexes
+            migrationBuilder.Up(Challenge.IX_Challenges_StatusId_StartDate);
+            migrationBuilder.Up(ChallengeParticipant.IX_ChallengeParticipants_ChallengeId_UserId);
+            migrationBuilder.Up(ChallengeParticipant.IX_ChallengeParticipants_StatusId_CompletedOn);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Down(Challenge.IX_Challenges_StatusId_StartDate);
+            migrationBuilder.Down(ChallengeParticipant.IX_ChallengeParticipants_ChallengeId_UserId);
+            migrationBuilder.Down(ChallengeParticipant.IX_ChallengeParticipants_StatusId_CompletedOn);
+        }
+    }
+}
