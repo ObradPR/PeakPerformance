702fe0c0c937bb6500c793e2aeedad531a2e2219
ObradPR <obradpualic@gmail.com>
2024-12-03 23:59:52 +0100

    PEAK-30 - HUB Dashboard - left menu item hover + active

--- Application/Source/PeakPerformance.Client/Workspace/src/app/pages/hub/left-menu/left-menu.component.ts
+++ Application/Source/PeakPerformance.Client/Workspace/src/app/pages/hub/left-menu/left-menu.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-left-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './left-menu.component.html',
   styleUrl: './left-menu.component.scss'
 })
