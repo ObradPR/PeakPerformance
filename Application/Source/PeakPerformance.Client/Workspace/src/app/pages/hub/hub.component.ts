d4aec890e2710c54f130549978adf3b0ce5692e6
ObradPR <obradpualic@gmail.com>
2024-12-04 23:31:29 +0100

    PEAK-30 - HUB Dashboard - Breadcrumb

--- Application/Source/PeakPerformance.Client/Workspace/src/app/pages/hub/hub.component.ts
+++ Application/Source/PeakPerformance.Client/Workspace/src/app/pages/hub/hub.component.ts
@@ -4,17 +4,28 @@ import { AccountSetupComponent } from './account-setup/account-setup.component';
 import { LeftMenuComponent } from "./left-menu/left-menu.component";
 import { UserPanelComponent } from "./user-panel/user-panel.component";
 import { RouterOutlet } from '@angular/router';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-hub',
   standalone: true,
-  imports: [AccountSetupComponent, LeftMenuComponent, UserPanelComponent, RouterOutlet],
+  imports: [
+    AccountSetupComponent,
+    LeftMenuComponent,
+    UserPanelComponent,
+    RouterOutlet,
+    BreadcrumbModule
+  ],
   templateUrl: './hub.component.html',
   styleUrl: './hub.component.scss',
 })
 export class HUBComponent implements OnInit {
+  breadcrumbItemHome: MenuItem = { icon: 'pi pi-home', routerLink: '/' };
 
-  constructor(public sharedService: SharedService) { }
+  constructor(public sharedService: SharedService) {
+    this.sharedService.pushBreadcrumbItem({ label: 'Dashboard', routerLink: '/hub' });
+  }
 
   ngOnInit(): void { }
 }
--- Application/Source/PeakPerformance.Client/Workspace/src/app/services/shared.service.ts
+++ Application/Source/PeakPerformance.Client/Workspace/src/app/services/shared.service.ts
@@ -1,14 +1,31 @@
 import { Injectable, signal } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SharedService {
-  private fromSignup = signal<boolean>(false);
+  // Account setup
 
+  private fromSignup = signal<boolean>(false);
   readonly fromSignupSignal = this.fromSignup.asReadonly();
 
   setFromSignupSignal(value: boolean) {
     this.fromSignup.set(value);
+  }
+
+  // Breadcrumb
+
+  private breadcrumbItems = signal<MenuItem[]>([]);
+  readonly breadCrumbItemsSignal = this.breadcrumbItems.asReadonly();
+
+  pushBreadcrumbItem(item: MenuItem) {
+    const currentArr = this.breadcrumbItems();
+    const idx = currentArr.indexOf((_: MenuItem) => _.label === item.label);
+
+    if (idx !== -1)
+      this.breadcrumbItems.set(currentArr.slice(0, idx + 1));
+    else
+      this.breadcrumbItems.set([...currentArr, item]);
   }
 }
