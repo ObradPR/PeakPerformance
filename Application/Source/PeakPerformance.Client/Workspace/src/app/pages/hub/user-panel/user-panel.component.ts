06c6e539b4a598ec491cc90c87a686d3555164b5
ObradPR <obradpualic@gmail.com>
2024-12-05 20:05:37 +0100

    PEAK-30 - HUB Dashboard - User panel - UI - Notification bell + Profile details

--- Application/Source/PeakPerformance.Client/Workspace/src/app/pages/hub/user-panel/user-panel.component.ts
+++ Application/Source/PeakPerformance.Client/Workspace/src/app/pages/hub/user-panel/user-panel.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { BadgeModule } from 'primeng/badge';
 
 @Component({
   selector: 'app-user-panel',
   standalone: true,
-  imports: [],
+  imports: [BadgeModule],
   templateUrl: './user-panel.component.html',
   styleUrl: './user-panel.component.scss'
 })
 export class UserPanelComponent {
-
+  notificationBadgeValue: string = '1';
 }
