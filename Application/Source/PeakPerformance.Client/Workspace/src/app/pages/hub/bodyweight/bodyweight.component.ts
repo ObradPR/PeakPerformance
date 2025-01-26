76d5df879c36b024addb340353d788c7621699b8
ObradPR <obradpualic@gmail.com>
2025-01-26 23:52:45 +0100

    PEAK-83 - HUB Bodyweight - Page - UI tabbed section for bodyweights part 1

--- Application/Source/PeakPerformance.Client/Workspace/src/app/pages/hub/bodyweight/bodyweight.component.ts
+++ Application/Source/PeakPerformance.Client/Workspace/src/app/pages/hub/bodyweight/bodyweight.component.ts
@@ -1,15 +1,16 @@
-import { Component, effect, OnDestroy, OnInit, Provider } from '@angular/core';
+import { Component, effect, OnDestroy, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Chart } from 'chart.js/auto';
 import { DateTime } from 'luxon';
 import { DropdownChangeEvent, DropdownModule } from 'primeng/dropdown';
+import { eChartTimespan } from '../../../_generated/enums';
 import { IEnumProvider, IPagingResult, IWeightDto, IWeightSearchOptions } from '../../../_generated/interfaces';
+import { Providers } from '../../../_generated/providers';
 import { WeightController } from '../../../_generated/services';
 import { BodyweightService } from '../../../services/bodyweight.service';
 import { ModalService } from '../../../services/modal.service';
 import { SharedService } from '../../../services/shared.service';
-import { Providers } from '../../../_generated/providers';
-import { eChartTimespan, eMeasurementUnit } from '../../../_generated/enums';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 enum eChartTarget {
   Bodyweight = 1,
@@ -20,7 +21,11 @@ type TChartTarget = 'weight' | 'bodyFatPercentage';
 @Component({
   selector: 'app-bodyweight',
   standalone: true,
-  imports: [DropdownModule, FormsModule],
+  imports: [
+    DropdownModule,
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './bodyweight.component.html',
   styleUrl: './bodyweight.component.scss'
 })
@@ -48,7 +53,29 @@ export class BodyweightComponent implements OnInit, OnDestroy {
   currentBodyFat: number;
   currentGoal: number;
 
+  selectedTab: number = 0;
+  tabs = [
+    {
+      label: 'Bodyweights',
+      icon: 'M5,2H19A2,2 0 0,1 21,4V20A2,2 0 0,1 19,22H5A2,2 0 0,1 3,20V4A2,2 0 0,1 5,2M12,4A4,4 0 0,0 8,8H11.26L10.85,5.23L12.9,8H16A4,4 0 0,0 12,4M5,10V20H19V10H5Z',
+      iconTitle: 'scale-bathroom'
+    },
+    {
+      label: 'Goals',
+      icon: 'M16,11.78L20.24,4.45L21.97,5.45L16.74,14.5L10.23,10.75L5.46,19H22V21H2V3H4V17.54L9.5,8L16,11.78Z',
+      iconTitle: 'chart-line'
+    }
+  ];
 
+  tableRows = [
+    { date: '2025-01-01', bodyweight: '70kg', bodyFat: '15%' },
+    { date: '2025-01-02', bodyweight: '70.5kg', bodyFat: '14%' },
+    { date: '2025-01-03', bodyweight: '71kg', bodyFat: '14.5%' },
+    { date: '2025-01-04', bodyweight: '71.5kg', bodyFat: '14%' },
+    { date: '2025-01-05', bodyweight: '72kg', bodyFat: '13.8%' },
+    { date: '2025-01-06', bodyweight: '72.3kg', bodyFat: '13.5%' },
+    { date: '2025-01-07', bodyweight: '72.8kg', bodyFat: '13.2%' }
+  ];
 
   constructor(
     private sharedService: SharedService,
@@ -66,6 +93,10 @@ export class BodyweightComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void { }
+
+  onSelectTab(idx: number): void {
+    this.selectedTab = idx;
+  }
 
   onTimespanChange = () => this.getBodyweights();
   onTargetChange(event: DropdownChangeEvent) {
